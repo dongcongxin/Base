58f074ed379dbb5b9c1b9e055bb43c58d7d785f7
dongxincong <1414423031@qq.com>
2017-04-07 17:57:31 +0800

    Encapsulation for elements

--- js/Base.js
+++ js/Base.js
@@ -1,11 +1,32 @@
 /**
  * Created by windows on 2017/3/29.
  */
+/*
+*  创建javascript库
+*  每个项目为了提高开发效率,我们需要创建一个用来存放大量的重复调用的代码
+*  说白了就是把常用的js代码片段组织到一起
+*  名字取得简短且富有意义
+* /
 window.onload=function(){
-    //获取当前日期
-    var d = new Date();
-    var f = d.getFullYear();
-    var birth = 1995;
-    document.write("董新从的年龄是:"+parseInt(f-birth));
+/*d对象式*/
+    var Base = {
+       getId:function(id){
+        return document.getElementById(id);
+       },
+       getName:function(name){
+        return document.getElementById(name);
+       },
+       getTagName:function(tag){
+        return document.getElementById(tag);
+       }
+    }
+
+
+
+
+
+
+
 }
+
 
